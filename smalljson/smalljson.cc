72b26ade8e319b1b765302341dd4b8a61a3a2fb3
dongliushi <2445264666@qq.com>
2022-06-30 19:17:43 +0800

    refine Value,Array,Object and Parser

--- smalljson/smalljson.cc
+++ smalljson/smalljson.cc
@@ -1,4 +1,5 @@
 #include "smalljson.h"
+#include <cassert>
 #include <iostream>
 
 namespace smalljson {
@@ -45,6 +46,341 @@ void Object::print() {
 void Array::print() {
   for (auto &x : array_data_) {
     x.print();
+  }
+}
+
+Value Parser::parseStart() {
+  skipWhiteSpace();
+  Value ret;
+  switch (*cur_) {
+  case '{':
+    ret = parseObject();
+    break;
+  case '[':
+    ret = parseArray();
+    break;
+  default:
+    throw Exception(Exception::ParseError::NOT_JSON);
+    break;
+  }
+  skipWhiteSpace();
+  if (*cur_ != '\0')
+    throw Exception(Exception::ParseError::ROOT_NOT_ONE);
+  return ret;
+}
+
+void Parser::skipWhiteSpace() {
+  while (cur_ != end_ &&
+         (*cur_ == ' ' || *cur_ == '\n' || *cur_ == '\r' || *cur_ == '\t'))
+    cur_++;
+}
+
+void Parser::skipDigit() {
+  while (cur_ != end_ && std::isdigit(*cur_))
+    cur_++;
+}
+
+Value Parser::parseObject() {
+  assert(*cur_ == '{');
+  cur_++;
+  skipWhiteSpace();
+  if (*cur_ == '}') {
+    cur_++;
+    return Object();
+  }
+  Object::object_t object_data;
+  while (true) {
+    skipWhiteSpace();
+    std::string key = parseRawString();
+    skipWhiteSpace();
+    if (*cur_ != ':')
+      throw Exception(Exception::ParseError::MISS_COLON);
+    cur_++;
+    skipWhiteSpace();
+    Value value = parseValue();
+    skipWhiteSpace();
+    object_data.emplace(unescapeJson(key), std::move(value));
+    if (*cur_ == ',') {
+      cur_++;
+    } else if (*cur_ == '}') {
+      cur_++;
+      break;
+    } else {
+      throw Exception(Exception::ParseError::LACK_COMMA_OR_BRACE);
+    }
+  }
+  return Object(std::move(object_data));
+}
+
+Value Parser::parseArray() {
+  assert(*cur_ == '[');
+  cur_++;
+  skipWhiteSpace();
+  if (*cur_ == ']') {
+    cur_++;
+    return Array();
+  }
+  Array::array_t array_data;
+  while (true) {
+    skipWhiteSpace();
+    Value value = parseValue();
+    skipWhiteSpace();
+    array_data.emplace_back(std::move(value));
+    if (*cur_ == ',') {
+      cur_++;
+    } else if (*cur_ == ']') {
+      cur_++;
+      break;
+    } else {
+      throw Exception(Exception::ParseError::LACK_COMMA_OR_BRACKET);
+    }
+  }
+  return Array(std::move(array_data));
+}
+
+Value Parser::parseValue() {
+  switch (*cur_) {
+  case 't':
+  case 'f':
+    return parseBoolean();
+  case 'n':
+    return parseNull();
+  case '"':
+    return parseString();
+  case '[':
+    return parseArray();
+  case '{':
+    return parseObject();
+  default:
+    break;
+  }
+  if (*cur_ == '-' || std::isdigit(*cur_)) {
+    return parseNumber();
+  }
+  throw Exception(Exception::ParseError::BAD_VALUE);
+  return nullptr;
+}
+
+std::string Parser::parseRawString() {
+  if (*cur_ != '"') {
+    throw Exception(Exception::ParseError::BAD_KEY);
+  }
+  cur_++;
+  iterator_t old_cur = cur_;
+  while (cur_ != end_) {
+    if (*cur_ == '\\') {
+      cur_++;
+      if (cur_ == end_)
+        throw Exception(Exception::ParseError::BAD_ESCAPE);
+      switch (*cur_) {
+      case '"':
+      case '\\':
+      case '/':
+      case 't':
+      case 'r':
+      case 'n':
+      case 'u':
+      case 'b':
+      case 'f':
+        cur_++;
+        break;
+      default:
+        throw Exception(Exception::ParseError::BAD_ESCAPE);
+        break;
+      }
+    } else if (*cur_ == '"') {
+      cur_++;
+      break;
+    } else {
+      cur_++;
+    }
+  }
+  if (cur_ == end_) {
+    throw Exception(Exception::ParseError::JSON_LENGTH);
+  }
+  return std::string(old_cur, cur_ - 1);
+}
+
+Value Parser::parseString() {
+  std::string str = parseRawString();
+  return Value(Value::ValueType::String, std::move(str));
+}
+
+Value Parser::parseBoolean() {
+  assert(*cur_ == 't' || *cur_ == 'f');
+  std::string str;
+  switch (*cur_) {
+  case 't':
+    str.assign(cur_, cur_ + 4 <= end_ ? cur_ + 4 : end_);
+    if (str == "true") {
+      cur_ += 4;
+      return Value(Value::ValueType::Boolean, std::move(str));
+    }
+    break;
+  case 'f':
+    str.assign(cur_, cur_ + 5 <= end_ ? cur_ + 5 : end_);
+    if (str == "false") {
+      cur_ += 5;
+      return Value(Value::ValueType::Boolean, std::move(str));
+    }
+    break;
+  default:
+    break;
+  }
+  throw Exception(Exception::ParseError::BAD_BOOLEAN);
+  return nullptr;
+}
+
+Value Parser::parseNull() {
+  assert(*cur_ == 'n');
+  std::string str(cur_, cur_ + 4 <= end_ ? cur_ + 4 : end_);
+  if (str == "null") {
+    cur_ += 4;
+    return Value();
+  }
+  throw Exception(Exception::ParseError::BAD_NULL);
+  return nullptr;
+}
+
+Value Parser::parseNumber() {
+  iterator_t old_cur = cur_;
+  if (*cur_ == '-') {
+    cur_++;
+  }
+  if (!std::isdigit(*cur_)) {
+    throw Exception(Exception::ParseError::BAD_NUMBER);
+  }
+  if (cur_ != end_ && *cur_ == '0' && cur_ + 1 != end_ &&
+      std::isdigit(*(cur_ + 1))) {
+    throw Exception(Exception::ParseError::BAD_NUMBER);
+  }
+  skipDigit();
+  switch (*cur_) {
+  case '.':
+    cur_++;
+    skipDigit();
+    break;
+  case 'e':
+  case 'E':
+    cur_++;
+    if (cur_ == end_) {
+      throw Exception(Exception::ParseError::BAD_NUMBER);
+    }
+    if (*cur_ == '+' || *cur_ == '-') {
+      cur_++;
+    }
+    if (!std::isdigit(*cur_)) {
+      throw Exception(Exception::ParseError::BAD_NUMBER);
+    }
+    skipDigit();
+  default:
+    break;
+  }
+  return Value(Value::ValueType::Number, std::string(old_cur, cur_));
+}
+
+std::string escapeJson(const std::string &str) {
+  std::string escapeStr(str);
+  std::string replaceStr[7] = {R"(\n)", R"(\t)", R"(\\)", R"(\b)",
+                               R"(\r)", R"(\")", R"(\f)"};
+  for (size_t idx = 0; idx < escapeStr.size(); idx++) {
+    size_t replaceIdx = 0;
+    switch (escapeStr[idx]) {
+    case '\n':
+      replaceIdx = 0;
+      break;
+    case '\t':
+      replaceIdx = 1;
+      break;
+    case '\\':
+      replaceIdx = 2;
+      break;
+    case '\b':
+      replaceIdx = 3;
+      break;
+    case '\r':
+      replaceIdx = 4;
+      break;
+    case '\"':
+      replaceIdx = 5;
+      break;
+    case '\f':
+      replaceIdx = 6;
+    default:
+      continue;
+    }
+    escapeStr.replace(idx, 1, replaceStr[replaceIdx]);
+    idx++;
+  }
+  return escapeStr;
+}
+
+std::string unescapeJson(const std::string &str) {
+  std::string unescapeStr(str);
+  std::string replaceStr[7] = {"\n", "\t", "\\", "\b", "\r", "\"", "\f"};
+  for (size_t idx = 0; idx < unescapeStr.size(); idx++) {
+    if (unescapeStr[idx] != '\\')
+      continue;
+    size_t replaceIdx = 0;
+    switch (unescapeStr[idx + 1]) {
+    case 'n':
+      replaceIdx = 0;
+      break;
+    case 't':
+      replaceIdx = 1;
+      break;
+    case '\\':
+      replaceIdx = 2;
+      break;
+    case 'b':
+      replaceIdx = 3;
+      break;
+    case 'r':
+      replaceIdx = 4;
+      break;
+    case '"':
+      replaceIdx = 5;
+      break;
+    case 'f':
+      replaceIdx = 6;
+    default:
+      break;
+    }
+    unescapeStr.replace(idx, 2, replaceStr[replaceIdx]);
+  }
+  return unescapeStr;
+}
+
+const char *Exception::errorToStr() const {
+  switch (err_) {
+  case ParseError::NOT_JSON:
+    return "format not json";
+  case ParseError::ROOT_NOT_ONE:
+    return "root not one";
+  case ParseError::MISS_COLON:
+    return "miss colon";
+  case ParseError::MISS_VALUE:
+    return "miss value";
+  case ParseError::LACK_COMMA_OR_BRACE:
+    return "lack ',' or '}'";
+  case ParseError::LACK_COMMA_OR_BRACKET:
+    return "lack ',' or ']'";
+  case ParseError::BAD_KEY:
+    return "bad key";
+  case ParseError::BAD_VALUE:
+    return "bad value";
+  case ParseError::JSON_LENGTH:
+    return "json format ";
+  case ParseError::BAD_ESCAPE:
+    return "bad escape";
+  case ParseError::BAD_BOOLEAN:
+    return "bad boolean";
+  case ParseError::BAD_NULL:
+    return "bad null";
+  case ParseError::BAD_NUMBER:
+    return "bad number";
+  default:
+    return "other error";
   }
 }
 
--- smalljson/smalljson.h
+++ smalljson/smalljson.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <map>
 #include <memory>
 #include <string>
@@ -46,6 +47,14 @@ public:
   Value &operator=(Value &&rhs) = default;
   void print();
 
+public:
+  template <typename... Args>
+  Value(ValueType type, Args &&...args)
+      : type_(type), value_data_(std::forward<Args>(args)...) {
+    static_assert(std::is_constructible<value_t, Args...>::value,
+                  "template args error");
+  }
+
 private:
   void deepCopy(const value_t &rhs);
   ValueType type_;
@@ -60,6 +69,8 @@ public:
   Object() = default;
   Object(const Object &rhs) = default;
   Object(Object &&rhs) noexcept = default;
+  Object(const object_t &object_data) : object_data_(object_data) {}
+  Object(object_t &&object_data) : object_data_(std::move(object_data)) {}
   Object(std::initializer_list<object_t::value_type> init_list)
       : object_data_(init_list) {}
   Object &operator=(const Object &rhs) = default;
@@ -78,6 +89,8 @@ public:
   Array() = default;
   Array(const Array &rhs) = default;
   Array(Array &&rhs) noexcept = default;
+  Array(const array_t &array_data) : array_data_(array_data) {}
+  Array(array_t &&array_data) : array_data_(std::move(array_data)) {}
   Array(std::initializer_list<array_t::value_type> init_list)
       : array_data_(init_list) {}
   Array &operator=(const Array &rhs) = default;
@@ -89,4 +102,59 @@ private:
   array_t array_data_;
 };
 
+class Parser {
+public:
+  typedef std::string::const_iterator iterator_t;
+  static Value parse(const std::string &json_data) {
+    return Parser(json_data).parseStart();
+  }
+
+private:
+  Parser(const std::string &json_data)
+      : cur_(json_data.begin()), end_(json_data.end()) {}
+
+  Value parseStart();
+  Value parseObject();
+  Value parseArray();
+  Value parseValue();
+  Value parseString();
+  Value parseBoolean();
+  Value parseNumber();
+  Value parseNull();
+  std::string parseRawString();
+  void skipWhiteSpace();
+  void skipDigit();
+
+private:
+  iterator_t cur_, end_;
+};
+
+class Exception : public std::exception {
+public:
+  enum class ParseError {
+    NOT_JSON,
+    ROOT_NOT_ONE,
+    MISS_COLON,
+    MISS_VALUE,
+    LACK_COMMA_OR_BRACE,
+    LACK_COMMA_OR_BRACKET,
+    BAD_KEY,
+    BAD_VALUE,
+    JSON_LENGTH,
+    BAD_ESCAPE,
+    BAD_BOOLEAN,
+    BAD_NULL,
+    BAD_NUMBER,
+  };
+  explicit Exception(ParseError err) : err_(err) {}
+  const char *what() const noexcept { return errorToStr(); }
+
+private:
+  const char *errorToStr() const;
+  ParseError err_;
+};
+
+std::string escapeJson(const std::string &str);
+
+std::string unescapeJson(const std::string &str);
 } // namespace smalljson
